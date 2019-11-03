c58b434366117ca18ae8c798ff904d49edae6d05
Laura Pognante <lalipognante@gmail.com>
2019-11-02 23:53:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+
+
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/config.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const classes = makeStyles(theme => ({
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+      marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+    card: {
+      border: '1px solid red',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    cardMedia: {
+      paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));

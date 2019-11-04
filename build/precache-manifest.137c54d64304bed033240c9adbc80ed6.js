1841f92cf935a7bd576b8ca3d2586878fb60a76c
Laura Pognante <lalipognante@gmail.com>
2019-11-03 21:46:43 -0300

    heroku commit

---
+++ build/precache-manifest.137c54d64304bed033240c9adbc80ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fb9b45c4a80d2fc518b858d503e828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dee9f764bb562d1f25e",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "105140505aed274682ac",
+    "url": "/static/css/main.a99e0b54.chunk.css"
+  },
+  {
+    "revision": "7dee9f764bb562d1f25e",
+    "url": "/static/js/2.594e9d75.chunk.js"
+  },
+  {
+    "revision": "105140505aed274682ac",
+    "url": "/static/js/main.2549bfd5.chunk.js"
+  },
+  {
+    "revision": "866d97eb5ce71bed7605",
+    "url": "/static/js/runtime-main.54b70b0d.js"
+  }
+]);
